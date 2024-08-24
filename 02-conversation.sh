a93850ac2f52a7862b1a45e70cd6b30171b432c4
Rajeshreddyk <rajeshreddy.k.315@gmail.com>
2024-08-24 10:36:32 +0530

    varibles

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+PERSON1=ramesh
+PERSON2=suresh
 
-echo "ramesh:: Hi bro , hwr"
-echo "suresh:: hi bro , fine ,hwr"
-echo "ramesh:: i am good,wahts going on"
-echo "suresh:: i stat=rted learning shell script Ramesh"
+echo "$PERSON1:: Hi bro , hwr"
+echo "$PERSON2:: hi bro , fine ,hwr"
+echo "$PERSON1:: i am good,wahts going on"
+echo "$PERSON2:: i stat=rted learning shell script $PERSON1"
