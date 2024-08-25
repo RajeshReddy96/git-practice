3bba343c94fa9479b7bbb895e749ed06595c54ad
Rajeshreddyk <rajeshreddy.k.315@gmail.com>
2024-08-25 15:56:50 +0530

    vars

---
+++ 03-varaibles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi ${PERSON2} , hwr"
+echo "$PERSON2:: hi $PERSON1. fine ,hwr"
+echo "$PERSON1:: i am good,${PERSON2}.whats going on"
+echo "$PERSON2:: i stat=rted learning shell script $PERSON1"
