9c8978247416b3f2f6685f826b736468a1dffe5a
Rajeshreddyk <rajeshreddy.k.315@gmail.com>
2024-08-26 18:18:34 +0530

    variables

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 USERID=$(id -u)
-
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then 
+        echo "please run this script with root priviliges"
+        exit 1
+    fi
+}
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
@@ -10,11 +16,13 @@ VALIDATE(){
         echo "$2 is success"
     fi
 }
-if [ $USERID -ne 0 ]
-then 
-    echo "please run this script with root priviliges"
-    exit 1
-fi
+
+CHECK_ROOT
+# if [ $USERID -ne 0 ]
+# then 
+#     echo "please run this script with root priviliges"
+#     exit 1
+# fi
 
 dnf list installed git
 
