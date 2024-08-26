b1ddb84c57ae5a79406a8faf10c86b5fbd8970af
Rajeshreddyk <rajeshreddy.k.315@gmail.com>
2024-08-26 17:38:24 +0530

    variables

--- 10-installation.sh
+++ 10-installation.sh
@@ -14,7 +14,31 @@ then
     echo "Git is not installed, going ito install it .."
     
     dnf install git -y
+    if [ $? -ne 0 ]
+    then 
+        echo "Git installation is not success..check it"
+        exit 
+    else
+        echo "Git installation is success"
+    fi
 else
     echo "Git is already installed, nothing to do...."
 
 fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+    echo "My Sql is not installed ... going to install"
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then 
+        echo "mysql is installation is failure.. plese check"
+        exit 1
+    else
+        echo "mysql installation is success"
+    fi
+else
+    echo "mysql is already installed...nothing to do"
+    
