d6c7887a4462dbc3f833a00cc487c16528f65b0d
Rajeshreddyk <rajeshreddy.k.315@gmail.com>
2024-08-27 17:20:56 +0530

    loop

---
+++ 15-redirectors.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCROPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root priveleges $N" &>>$LOG_FILE
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N" &>>$LOG_FILE
+    fi
+}
+USAGE(){
+    echo -e "$R USAGE:: $N sudo sh 15-redirectors.sh package 1 package 2 ..."
+    exit 1
+}
+
+echo "script started executing at: $date"
+CHECK_ROOT
+
+if [ $# -eq 0 ]
+then 
+    USAGE
+if
+for package in $@  #$@ refers to all arguments passed to it
+do 
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0 ]
+    then 
+         echo "$package is not installed, going to install it.." &>>$LOG_FILE
+    dnf install $package -y &>>$LOG_FILE
+    VALIDATE $? "Installing $package"
+else
+    echo -e "$package is already $Y installed.. nothing to do $N" &>>$LOG_FILE
+fi
+done 
